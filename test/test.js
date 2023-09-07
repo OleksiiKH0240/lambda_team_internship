927b0866d155d93b24009e3c88410bc5ade91cfa
OleksiiKhomenko <homenko0240@gmail.com>
2023-09-07 17:13:08 +0300

    correction commit

--- test/test.js
+++ test/test.js
@@ -4,25 +4,26 @@ const previousStateMap = { "currency": "start", city: "start", "frequency": "cit
 
 // console.log(1 in state);
 // const s = previousStateMap.city;
-// const s =  "hello" || process.env.botUrl;
-const s = [
-    {
-        "currencyCodeA": 840,
-        "currencyCodeB": 980,
-        "date": 1694034073,
-        "rateBuy": 36.65,
-        "rateCross": 0,
-        "rateSell": 37.4406
-    },
-    {
-        "currencyCodeA": 978,
-        "currencyCodeB": 980,
-        "date": 1694034073,
-        "rateBuy": 39.3,
-        "rateCross": 0,
-        "rateSell": 40.6504
-    }
-];
+const s =  undefined || "hi";
+console.log(s);
+// const s = [
+//     {
+//         "currencyCodeA": 840,
+//         "currencyCodeB": 980,
+//         "date": 1694034073,
+//         "rateBuy": 36.65,
+//         "rateCross": 0,
+//         "rateSell": 37.4406
+//     },
+//     {
+//         "currencyCodeA": 978,
+//         "currencyCodeB": 980,
+//         "date": 1694034073,
+//         "rateBuy": 39.3,
+//         "rateCross": 0,
+//         "rateSell": 40.6504
+//     }
+// ];
 
-console.log(s.filter(
-    (currRate) => currRate["currencyCodeA"] == 840 && currRate["currencyCodeB"] == 980));
+// console.log(s.filter(
+//     (currRate) => currRate["currencyCodeA"] == 840 && currRate["currencyCodeB"] == 980));
