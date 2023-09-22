486631caf70470dd8f156c958adba44eca028b3c
OleksiiKhomenko <homenko0240@gmail.com>
2023-09-22 03:18:42 +0300

    correction commit

--- test/test1.js
+++ test/test1.js
@@ -1,3 +1,4 @@
 let a = [1, 2, 3];
-console.log(Object.keys(a));
-console.log(a["0"]);
+// console.log(Object.keys(a));
+// console.log(a.find(value => value == 4));
+console.log("a" && "ab".split(" ")[1])
