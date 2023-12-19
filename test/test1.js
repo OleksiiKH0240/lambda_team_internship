398757026056579b349beae2bc568d8bd4f3bb8c
OleksiiKhomenko <homenko0240@gmail.com>
2023-12-19 17:53:15 +0200

    correction commit

--- test/test1.js
+++ test/test1.js
@@ -4,7 +4,7 @@ let o = new Map();
 o.set("a", 4);
 o.set("a", 5);
 // console.log(o.get("a"));
-let obj = { a: 1, b: 4 };
+let obj = { a: 1, b: 4, c: 5, d: 5 };
 
 const dateStr = new Date().toLocaleString("en-US", {
     hourCycle: "h23",
@@ -18,14 +18,43 @@ const testTime = new Date(Date.parse(dateStr));
 
 Object.defineProperty(Array.prototype, "multiply",
     {
-        value : function (multiplier = 10) { return this.map((val) => val * multiplier) }
+        value: function (multiplier = 10) { return this.map((val) => val * multiplier) }
     })
 
 // Array.prototype.multiply = function (multiplier: number = 10) { return this.map((val: number) => val * multiplier) };
 
 const c = [false, true];
 
-console.log(c.name?.letter);
+const filtered = Object.entries(obj).filter(([key, value]) => ["a", "c"].includes(key));
+const filteredObj = Object.fromEntries(filtered);
+
+console.log({a});
 
 // console.log(a.find(value => value == 4));
 // console.log("a" && "ab".split(" ")[1])
+
+try {
+    throw new SyntaxError();
+}
+catch (e) {
+    if (e instanceof TypeError){
+        console.log("type error");
+    } 
+    if (e instanceof SyntaxError) {
+        console.log("error");
+    }
+    // console.log(e);
+}
+let a1;
+a1 =  {"a1": 1, "b": 2}
+const l = []
+// l.push(...[4,5,6])
+a1["c"] = 5;
+
+const d = Object.fromEntries(l, )
+
+console.log(l.lenght)
+
+
+
+
